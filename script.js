a96c91e91c6969d0c1707b32296a1ed1fea16cbe
Dinesh <145418472+Dinesh-Mahendiran@users.noreply.github.com>
2023-12-24 12:55:31 +0530

    Add files via upload
    Basic web page

---
+++ script.js
@@ -0,0 +1,23 @@
+// let obj1={name:"Person 1",age:5};
+// let obj2={age:5,name:"Person 1"};
+
+
+// // To convert object to JSON
+
+
+// let json1=JSON.stringify(obj1);
+// console.log(json1);
+// let json2=JSON.stringify(obj2);
+// console.log(json2);
+
+// if(json1===json2)
+// {
+//     console.log("both are equal")
+// }
+// else
+// {
+//     console.log("both are different")
+// }
+
+
+
